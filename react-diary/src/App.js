2e514b6cf64115f078cc29163176a6c75e535653
MJ <mj632900@gmail.com>
2022-09-04 21:56:01 +0900

    최적화 : useCallback

--- react-diary/src/App.js
+++ react-diary/src/App.js
@@ -1,4 +1,4 @@
-import { useEffect, useMemo, useRef, useState } from "react";
+import { useCallback, useEffect, useMemo, useRef, useState } from "react";
 import "./App.css";
 import DiaryList from "./DiaryList";
 import DiaryEditor from "./DiaryEditor";
@@ -32,7 +32,7 @@ function App() {
         getData();
     }, []);
 
-    const onCreate = (author, content, emotion) => {
+    const onCreate = useCallback((author, content, emotion) => {
         const create_date = new Date().getTime();
         const newItem = {
             author,
@@ -42,8 +42,8 @@ function App() {
             id: dataId.current,
         };
         dataId.current += 1;
-        setData([newItem, ...data]);
-    };
+        setData((data) => [newItem, ...data]);
+    }, []);
 
     const onRemove = (targetId) => {
         const newDiaryList = data.filter((it) => it.id !== targetId);
