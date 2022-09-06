ab57dcda69fe5a29602711c080c499c62bf6015d
MJ <mj632900@gmail.com>
2022-09-06 23:50:07 +0900

    페이지 라우팅2 - React Router 응용

--- React/emotion-diary/src/pages/Edit.js
+++ React/emotion-diary/src/pages/Edit.js
@@ -1,8 +1,36 @@
+import { useNavigate, useSearchParams } from "react-router-dom";
+
 const Edit = () => {
+    const navigate = useNavigate();
+    const [searchParms, setSearchParms] = useSearchParams();
+
+    const id = searchParms.get("id");
+    console.log("id : ", id);
+
+    const mode = searchParms.get("mode");
+    console.log("mode : ", mode);
+
     return (
         <div>
             <h1>Edit</h1>
             <p>이 곳은 수정 페이지 입니다.</p>
+            <button onClick={() => setSearchParms({ who: "mj" })}>
+                QS 바꾸기
+            </button>
+            <button
+                onClick={() => {
+                    navigate("/home");
+                }}
+            >
+                Home 으로
+            </button>
+            <button
+                onClick={() => {
+                    navigate(-1);
+                }}
+            >
+                뒤로가기
+            </button>
         </div>
     );
 };
