dccdb05bdeba1390fdea1ca1e014dc3857cfe17f
MJ <mj632900@gmail.com>
2022-09-08 22:54:00 +0900

    페이지 구현 -홈 : header

--- React/emotion-diary/src/pages/Home.js
+++ React/emotion-diary/src/pages/Home.js
@@ -1,8 +1,38 @@
+import { useState } from "react";
+
+import MyHeader from "./../components/MyHeader";
+import Mybutton from "./../components/MyButton";
+
 const Home = () => {
+    const [curDate, setCurDate] = useState(new Date());
+    const headText = `${curDate.getFullYear()}년 ${curDate.getMonth() + 1}월`;
+
+    const increaseMonth = () => {
+        setCurDate(
+            new Date(
+                curDate.getFullYear(),
+                curDate.getMonth() + 1,
+                curDate.getDate()
+            )
+        );
+    };
+    const decreaseMonth = () => {
+        setCurDate(
+            new Date(
+                curDate.getFullYear(),
+                curDate.getMonth() - 1,
+                curDate.getDate()
+            )
+        );
+    };
+
     return (
         <div>
-            <h1>Home</h1>
-            <p>이 곳은 홈 입니다.</p>
+            <MyHeader
+                headText={headText}
+                leftChild={<Mybutton text={"<"} onClick={decreaseMonth} />}
+                rightChild={<Mybutton text={">"} onClick={increaseMonth} />}
+            />
         </div>
     );
 };
