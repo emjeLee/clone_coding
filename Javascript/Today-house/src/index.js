a6188734fd513d39f3b82273eaec57d3cd16791b
MJ <mj632900@gmail.com>
2022-10-06 23:17:58 +0900

    Index Page ToggleButton

---
+++ Javascript/Today-house/src/index.js
@@ -0,0 +1,13 @@
+const $sidebarToggle = document.querySelector(".side-bar-toggle");
+const $sidebar = document.querySelector(".side-bar");
+const $backDrop = document.querySelector(".back-drop");
+
+$sidebarToggle.addEventListener("click", () => {
+    $sidebar.classList.toggle("open");
+    $backDrop.style.display = "block";
+});
+
+$backDrop.addEventListener("click", () => {
+    $sidebar.classList.toggle("open");
+    $backDrop.style.display = "none";
+});
